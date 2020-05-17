5dbfa46481978a52bb49cb5a2c117f6a7847da4f
Harnet69 <harnet69@gmail.com>
2020-05-17 18:05:25 +0200

    Page 2 was implemented

---
+++ app/src/main/java/com/harnet/basiclayouts/controller/Page2.java
@@ -0,0 +1,16 @@
+package com.harnet.basiclayouts.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.harnet.basiclayouts.R;
+
+public class Page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+}
