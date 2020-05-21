ea49e6f8f066856ee2819b2a9bb8d488522a3359
Harnet69 <harnet69@gmail.com>
2020-05-21 10:13:36 +0200

    Link to French page works correctly

--- app/src/main/java/com/harnet/basiclayouts/controller/MainActivity.java
+++ app/src/main/java/com/harnet/basiclayouts/controller/MainActivity.java
@@ -1,34 +1,26 @@
 package com.harnet.basiclayouts.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import com.harnet.basiclayouts.R;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
-//    AudioThequeController audioThequeController = new AudioThequeController(this);
-//    public static final String BTNS_QUANTITY = "BTNS_QUANTITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        audioThequeController.addAllMediaWords(); // add all mediaFile from raw folder to List
-//        initRecyclerView();
 
+        View frenchModule = findViewById(R.id.frIcoView);
+        final Intent intent = new Intent(this, FrenchActivity.class);
+
+        frenchModule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
-
-//    // init recycler View
-//    public void initRecyclerView(){
-//        List<String> words = audioThequeController.getAudioTheque().getWords();
-//        List<MediaPlayer> files = audioThequeController.getAudioTheque().getMediaWords();
-//        RecyclerView recyclerView = findViewById(R.id.recyrclerView);
-//
-//        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, audioThequeController, words, files);
-//        recyclerView.setAdapter(adapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//    }
 }
