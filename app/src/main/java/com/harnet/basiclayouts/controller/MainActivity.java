3429549d213c540e5925b11617e6a94f2180a7cd
Harnet69 <harnet69@gmail.com>
2020-05-21 09:35:16 +0200

    Add flags

--- app/src/main/java/com/harnet/basiclayouts/controller/MainActivity.java
+++ app/src/main/java/com/harnet/basiclayouts/controller/MainActivity.java
@@ -9,26 +9,26 @@ import com.harnet.basiclayouts.R;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
-    AudioThequeController audioThequeController = new AudioThequeController(this);
-    public static final String BTNS_QUANTITY = "BTNS_QUANTITY";
+//    AudioThequeController audioThequeController = new AudioThequeController(this);
+//    public static final String BTNS_QUANTITY = "BTNS_QUANTITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        audioThequeController.addAllMediaWords(); // add all mediaFile from raw folder to List
-        initRecyclerView();
+//        audioThequeController.addAllMediaWords(); // add all mediaFile from raw folder to List
+//        initRecyclerView();
 
     }
 
-    // init recycler View
-    public void initRecyclerView(){
-        List<String> words = audioThequeController.getAudioTheque().getWords();
-        List<MediaPlayer> files = audioThequeController.getAudioTheque().getMediaWords();
-        RecyclerView recyclerView = findViewById(R.id.recyrclerView);
-
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, audioThequeController, words, files);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
+//    // init recycler View
+//    public void initRecyclerView(){
+//        List<String> words = audioThequeController.getAudioTheque().getWords();
+//        List<MediaPlayer> files = audioThequeController.getAudioTheque().getMediaWords();
+//        RecyclerView recyclerView = findViewById(R.id.recyrclerView);
+//
+//        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, audioThequeController, words, files);
+//        recyclerView.setAdapter(adapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//    }
 }
